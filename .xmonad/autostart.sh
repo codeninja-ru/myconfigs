06bfb9173d350d1a991975420f5a6d22a5731041
codeninja <vital@ubuntu-home.(none)>
2012-06-19 14:49:16 +1100

    background fix

--- .xmonad/autostart.sh
+++ .xmonad/autostart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 setxkbmap -layout "us,ru" -option "grp:caps_toggle"
-feh --bg-scale=/home/vital/.xmonad/wall/wallpaper-430554.jpg
+feh --bg-scale "/home/vital/.xmonad/wall/wallpaper-430554.jpg" &
+/home/vital/.dropbox-dist/dropboxd &
