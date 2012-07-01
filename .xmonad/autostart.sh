c1d72c3ddf4ec04683fb126c52b5bd85a085238d
codeninja <vital@ubuntu-home.(none)>
2012-07-01 21:59:07 +1100

    add tint2

--- .xmonad/autostart.sh
+++ .xmonad/autostart.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+HOME="/home/vital"
+
 setxkbmap -layout "us,ru" -option "grp:caps_toggle"
 feh --bg-scale "/home/vital/.xmonad/wall/wallpaper-430554.jpg" &
 /home/vital/.dropbox-dist/dropboxd &
+tint2 -c "$HOME/.xmonad/tint2rc"
