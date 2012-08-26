284e1d2b51b8f19074e05bcc5dceae2baf595eb7
codeninja <vital@ubuntu-home.(none)>
2012-08-26 14:44:09 +1100

    added xcompgr

--- .xmonad/autostart.sh
+++ .xmonad/autostart.sh
@@ -3,6 +3,7 @@
 HOME="/home/vital"
 
 setxkbmap -layout "us,ru" -option "grp:caps_toggle"
+xcompgr &
 feh --bg-scale "/home/vital/.xmonad/wall/wallpaper-430554.jpg" &
 /home/vital/.dropbox-dist/dropboxd &
 tint2 -c "$HOME/.xmonad/tint2rc" &
