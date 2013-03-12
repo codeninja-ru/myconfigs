a8e357f151ac2723786aa797bbd6e5fcafec0d56
codeninja <vital@ubuntu-home.(none)>
2013-03-12 22:22:44 +1100

    added parcellite

--- .xmonad/autostart.sh
+++ .xmonad/autostart.sh
@@ -7,4 +7,4 @@ xcompgr &
 feh --bg-scale "/home/vital/.xmonad/wall/wallpaper-430554.jpg" &
 /home/vital/.dropbox-dist/dropboxd &
 tint2 -c "$HOME/.xmonad/tint2rc" &
-parcellite # clipboard
+parcellite & # clipboard
