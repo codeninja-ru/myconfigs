613ef31d4ae82f260ca1527ac105d9c0055661e1
vital <vital@debian>
2015-02-04 21:40:38 +0300

    changed autostart

--- .xmonad/autostart.sh
+++ .xmonad/autostart.sh
@@ -3,8 +3,8 @@
 HOME="/home/vital"
 
 setxkbmap -layout "us,ru" -option "grp:caps_toggle,grp_led:scroll,compose:win"
-xcompgr &
+#xcompgr &
 feh --bg-scale "/home/vital/.xmonad/wall/wallpaper-430554.jpg" &
-/home/vital/.dropbox-dist/dropboxd &
-tint2 -c "$HOME/.xmonad/tint2rc" &
+#/home/vital/.dropbox-dist/dropboxd &
+#tint2 -c "$HOME/.xmonad/tint2rc" &
 parcellite & # clipboard
